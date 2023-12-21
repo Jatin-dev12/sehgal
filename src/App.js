8b9baeb5d3b598893b9592be0b70fcfc09f20e74
Jatin-dev12 <jatin@iosandweb.net>
2023-12-21 17:45:05 +0530

    new files

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
-import logo from './jj.jpg';
+
 import Header from './componets/Header';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-function App() {
+function App() {  
   return (
     <div className="App">
       <Header />
-      <header className="App-header">
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         
-          Edit <code>src/App.js</code> and save to reload.
-        </p> 
-        <button> jatin sehgal</button><br></br>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          jatin sehgal
-        </a>
-    
-        <section>
-        <h1>My First Component</h1>
-        <ol>
-        <li>Defining a Component</li>
-        <li>Components: UI Building Blocks</li>
-        <li>Using a Component</li>
-        </ol>
-        </section>
-        
-      </header>
     </div>
   );
 }
--- src/componets/Header.js
+++ src/componets/Header.js
@@ -2,34 +2,43 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import pic from '../12.png'
 
 function Header() {
   return (
     
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Container className="justify-content-center">
+    <Navbar.Brand href="#home">
+            <img
+              src={pic}             
+              width="50"
+              height="50"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+        <Nav className="justify-content-center">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Services</Nav.Link>
+          <Nav.Link href="#home">Portfolio</Nav.Link>
+          <Nav.Link href="#link">About Us</Nav.Link>
+          <Nav.Link href="#link">Contact Us</Nav.Link>
+              </Nav>
+              
+          
+      </Navbar.Collapse>
+      <Col xs="auto">
+            <Button type="submit"> 📞 9915841204</Button>
+          </Col>
+    </Container>
+  </Navbar>
+
+
   );
 }
 
