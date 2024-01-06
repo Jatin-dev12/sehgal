bcc2b55db856af2d7393c4773c5edb653f26a294
jatinxsehgal <130349292+jatinxsehgal@users.noreply.github.com>
2024-01-06 17:42:37 +0530

    Input Form Add

--- src/componets/Portfolio.js
+++ src/componets/Portfolio.js
@@ -3,54 +3,46 @@ import axios from 'axios';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { FilterMatchMode } from 'primereact/api';
-import { InputText } from 'primereact/inputtext';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
-import { AutoComplete } from 'primereact/autocomplete';
 
-     
 // I Wanna do in status row if value comes 5 then its aaprove .
 // other wise it will be show pendings.s
 
 function Portfolio() {
-  const url = 'https://buying.com/getStakeAlldata/key/12345';
+  const [searchTerm,setSearchTerm]=  useState("")
   const [data, setData] = useState([]);
-
   const fetchInfo = () => {
     return axios.get('https://buying.com/getStakeAlldata/key/12345')
-      .then((res) => setData(res.data));
+      .then((res) => setData(res.data))
   };
-  const [filters, setFilters] = useState({
+  const [filters, ] = useState({
     global: { value: null, matchmode: FilterMatchMode.CONTAINS },
   })
-
   useEffect(() => {
     fetchInfo();
   }, [])
   return (
     <div>
 
-      <InputText
+      {/* <InputText
         onInput={(e) =>
           setFilters({
             global: { value: e.target.value, matchmode: FilterMatchMode.CONTAINS }
-          })} />
+          })} /> */}
+          
+      <input type="text"
+      placeholder="Search.." 
+      onChange={event =>{
+      setSearchTerm(event.target.value)}} />      
 
 
-
-      <br></br>
-      <br></br>
-
-
-      <DataTable value={data} sortMode='multiple' filters={filters}
-
-      >
+      <DataTable value={data} sortMode='multiple' filters={filters} tableStyle={{ minWidth: '50rem' }}>
         <Column field='id' header='ID' sortable />
         <Column field='wallet_address' header='WALLET ADDRESS' sortable />
         <Column field='staking_date' header='STAKING DATE' sortable />
         <Column field='staking_expire' header='STAKING EXPIRE' sortable />
         <Column field='staking_amount' header='STAKING AMOUNT' sortable />
         <Column field='highlight_status' header='STATUS' sortable />
-
       </DataTable>
     </div>
   )
