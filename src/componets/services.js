b8163c6bca249a2a6181a6eed7ae9aaa7b9f9036
Jatin-dev12 <jatin@iosandweb.net>
2024-01-11 12:43:07 +0530

    post method is also done

--- src/componets/services.js
+++ src/componets/services.js
@@ -1,15 +1,12 @@
 import React, { useState, useEffect, useMemo } from 'react'
 import Table from 'react-bootstrap/Table';
 import axios from 'axios';
+import './Api.css'
 
 function Services() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [newName, setNewName] = useState('');
-  const [newEmail, setNewEmail] = useState('');
-  const [newGender, setNewGender] = useState('');
-  const [newStatus, setNewStatus] = useState('');
   const [token, setToken] = useState('d8181c3818c8a19a0d2f6a21a31aaa56fbdaf4812efd5fcbcec41a2b93bbe150');
 
   useEffect(() => {
@@ -50,53 +47,17 @@ function Services() {
     </thead>
   ), []);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await axios.post('https://gorest.co.in/public/v2/users', {
-        name: newName,
-        email: newEmail,
-        gender: newGender,
-        status: newStatus        
-      },
-    {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-       } );
-      
-      setData([...data, response.data]);
-      setNewName('');
-      setNewEmail('');
-      setNewGender('');
-      setNewStatus('');
-    } catch (error) {
-      console.error('Error posting data: ', error);
-      setError(error);
-    }
-  };
 
-  if (loading) {
-    return <p>Loading data...</p>;
-  }
-
-  if (error) {
-    if (error === 'The requested resource could not be found.') {
-      return <p>{error}</p>;
-    } else {
-      return <p>Error fetching data: {error.message}</p>;
-    }
-  }
 
   return (
     <div>
-      <h1>API LIST</h1>
+      <h1 className='no'>CONTACT FORM DATA</h1>
       <Table striped bordered hover>
         {tableHeaders}
         <tbody>
           {data.map((item, index) => (
             <tr key={index}>
-              <td>{item.id}</td>
+              <td>{index+1}</td>
               <td>{item.name}</td>
               <td>{item.email}</td>
               <td>{item.gender}</td>
@@ -105,34 +66,6 @@ function Services() {
           ))}
         </tbody>
       </Table>
-
-      <h2>Add User</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={newName} onChange={(event) => setNewName(event.target.value)} />
-        </label>
-        <label>
-          Email:
-          <input type="email" value={newEmail} onChange={(event) => setNewEmail(event.target.value)} />
-        </label>
-        <label>
-          Gender:
-          <select value={newGender} onChange={(event) => setNewGender(event.target.value)}>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Other</option>
-          </select>
-        </label>
-        <label>
-          Status:
-          <select value={newStatus} onChange={(event) => setNewStatus(event.target.value)}>
-            <option value="active">Active</option>
-            <option value="inactive">I  nactive</option>
-          </select>
-        </label>
-        <button type="submit">Add</button>
-      </form>
     </div>
   );
 }
